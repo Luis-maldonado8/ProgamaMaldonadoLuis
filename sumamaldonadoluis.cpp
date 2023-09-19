ecf63e723a4a0adc497d92bc68080244e707be21
Luis-maldonado8 <144960096+Luis-maldonado8@users.noreply.github.com>
2023-09-19 13:14:38 -0500

    Add files via upload

---
+++ sumamaldonadoluis.cpp
@@ -0,0 +1,22 @@
+/* Programa: Suma.cpp
+   Autot: Luis Maldonado
+   Fecha: 2023-09-14
+   Descripcuon: Suma dos numeros
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	float x,y,z;
+	cout<<"Ingrese x :"; cin>>x;
+	cout<<"Ingrese y :"; cin>>y;
+	//aqui realiza la suma
+	z=x+y;
+	cout<<"La suma de "<<x<<"+"<<y<<"="<<z<<endl;
+	return (0);
+
+}
+	
